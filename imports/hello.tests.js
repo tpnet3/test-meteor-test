ce9884db980738bdf932530614a2d113fc05941b
Gyeong-Sik Park <tpnet3@gmail.com>
2017-09-22 09:34:46 +0900

    meteor test init

---
+++ imports/hello.tests.js
@@ -0,0 +1,8 @@
+import { Meteor } from 'meteor/meteor';
+import { assert } from 'meteor/practicalmeteor:chai';
+
+describe('Hello tests', () => {
+  it('can delete owned task', () => {
+    assert.equal(2+2, 4);
+  });
+});
